7f94b4cd745484cfede943fcb3f44251d27fa3b0
Stanislav93 <stas.stecenko@ukr.net>
2020-07-10 23:44:39 +0300

    start

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=platisua.mysql.tools;dbname=platisua_obnalonomika',
+    'username' => 'platisua_obnalonomika',
+    'password' => '8cTB^dj76%1',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
